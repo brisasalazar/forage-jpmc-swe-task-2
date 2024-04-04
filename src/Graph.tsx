68f9ced5cd2574520847058d1a996324ae737581
brisasalazar <brisal.853@gmail.com>
2024-04-03 22:29:45 -0700

    changes to complete task 2

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import './Graph.css';
+import { timeStamp } from 'console';
 
 /**
  * Props declaration for <Graph />
@@ -14,7 +15,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -32,10 +33,17 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
-
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    elem.setAttribute('view', 'y_line');
+    elem.setAttribute('row-pivots', '["timestamp"]');
+    elem.setAttribute('column-pivots', '["stock"]');
+    elem.setAttribute('columns','["top_ask_price"]');
+    elem.setAttribute('aggregates', `{"stock": "distinct count", 
+                                    "top_ask_price": "avg",
+                                    "top_bid_price" : "avg", 
+                                    "timestamp": "distinct count"}`);
     const schema = {
-      stock: 'string',
+      stock: 'string',  
       top_ask_price: 'float',
       top_bid_price: 'float',
       timestamp: 'date',
